80be1f2895f1422d7bfb2b2da3af82bc077c591a
sumit dhakal <sumudhakal94@gmail.com>
2026-06-16 22:14:12 +0545

    usecallback

--- JS/basic.js
+++ JS/basic.js
@@ -1,24 +1,38 @@
-const accountId = 129
-let accountName = "sumit dhakal";
-let accountEmail = "sumit@example.com";
-let accountPassword = "password123";
-var accountPhone = "1322324242";
+// const accountId = 129
+// let accountName = "sumit dhakal";
+// let accountEmail = "sumit@example.com";
+// let accountPassword = "password123";
+// var accountPhone = "1322324242";
 
-function createAccount() {
-    console.log("account created successfully");
-} 
+// function createAccount() {
+//     console.log("account created successfully");
+// } 
 
-let passwordLength = 8;
+// let passwordLength = 8;
 
-if(passwordLength < 8) {
-console.log("password must be at least 8 characters long");
+// if(passwordLength < 8) {
+// console.log("password must be at least 8 characters long");
+// }
+
+// else(passwordLength >= 8); {
+//     console.log("password is valid");
+// }
+// /*
+// don't use var for variable declaration as it can lead to unexpected behavior due to its function scope. Instead, use let or const for block-scoped variables.
+// */
+// console.table({accountId, accountName, accountEmail, accountPassword, accountPhone})
+
+import React, {useEffect, useState } from 'react'
+
+export default  function list({getItems}) {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        setItems(getItems())
+        console.log('updating Items')
+    }, [getItems])
+
+return items.map(item => <div key={item}> {item}
+
+</div>)
 }
-
-else(passwordLength >= 8); {
-    console.log("password is valid");
-}
-/*
-don't use var for variable declaration as it can lead to unexpected behavior due to its function scope. Instead, use let or const for block-scoped variables.
-*/
-console.table({accountId, accountName, accountEmail, accountPassword, accountPhone})
-
---
+++ JS/operations.js
@@ -0,0 +1,33 @@
+import { useCallback, useState } from "react";
+import List from './basic'
+
+export default function Operation() {
+    const [number, setNumber] = useState(1)
+    const [dark, setDark] = useState(false)
+
+    const getItems = useCallback(() => {
+        return [number, number + 1, number + 2]
+    }, [number])
+
+    const them = {
+        backgroundColor: dark ? '#999' : '#111',
+        Color: dark ? '#111' : '#999'
+    }
+
+    return(
+        <div style={them}>
+        <input
+            type = "number"
+            value={number}
+            onChange={e => setNumber(parseInt(e.target.value))}
+            />
+
+            <button onClick={() => setDark(prevDark => !prevDark)}>
+                click button
+            </button>
+        <List getItems= {getItems}/>
+
+        </div>
+    )
+    
+}
