906acdcbd87d03710d5d034a79e7a1bd422a4e2c
sumit dhakal <sumudhakal94@gmail.com>
2026-08-23 23:53:05 +0545

    configure URL

--- JS/server.js
+++ JS/server.js
@@ -2,19 +2,30 @@ const http = require('http');
 const fs = require('fs');
 const { error, log } = require('console');
 
-const PersonalServer = http.createServer((req , res) =>{
+// const PersonalServer = http.createServer((req , res) =>{
+//     const log = `${Date.now()} ${req.url}: new user log \n`;
 
-    fs.appendFile('UserLog.txt', log, (err, data ) =>{
-    res.end("hi iam server")
-    const log = `${Date.now()}: new user log`;
+//     fs.appendFile('Log.txt', log, (err , data) => {
+// const con = (req.url)
+//         if('/contact') {
+//             console.log('connect with us')
+//         }
+
+//         else('/project')
+//             console.log('200+project')
+        
+
+//     res.end("hi iam server")
 
 
-    } )
+//     } )
 
-});
+// });
 
-    // console.log('incomming request ');
-    // console.log(req.headers); 
+//     // console.log('incomming request ');
+//     // console.log(req.headers); 
 
 
-PersonalServer.listen(3000, () => console.log('server initalize'));
+// PersonalServer.listen(3000, () => console.log('server initalize'));
+
+fs.writeFileSync('systeminfo.txt', 'hello this is the authorized resellar of apple')
