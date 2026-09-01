6ca06cece5cce33782bd06ae0d99064cd419a1a5
sumit dhakal <sumudhakal94@gmail.com>
2026-09-01 23:59:23 +0545

    json data featch

--- rest/index.js
+++ rest/index.js
@@ -1,9 +1,22 @@
 const express = require("express")
+const User = require("./MOCK_DATA.json")
 
 const app = express();
 const PORT = 8000; 
 
 //route
+app.get ("/users", (req, res) =>{
+    const userName = `
+    
+    <ul>
+    ${User.map((user) => `<li>${user.first_name}</li>`).join("")
+
+}
+    </ul>
+
+    `;
+    res.send(userName);
+});
 
 
 
